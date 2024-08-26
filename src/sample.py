f98c4132015039accd6dc07ddc9eee654a1b42d3
Boxwala, Adnan (EFS-GV2) <adnan.boxwala@efs-auto.de>
2024-08-26 11:12:18 +0200

    updated test summary, python script

--- src/sample.py
+++ src/sample.py
@@ -1,2 +1,5 @@
 def add(a: int, b: int) -> int:
     return a + b
+
+if __name__ == "__main__":
+    print("Exploring GitHub Actions...")
--- tests/test_sample.py
+++ tests/test_sample.py
@@ -1,5 +1,8 @@
 import pytest
 from src.sample import add
 
-def test_sample():
+def test_sample_success():
     assert add(5, 5) == 10
+
+def test_sample_failure():
+    assert add(5, 5) == 55
