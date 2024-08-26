acc0eadfc8ae6fd44fec8bdf25c25079aeed364b
Boxwala, Adnan (EFS-GV2) <adnan.boxwala@efs-auto.de>
2024-08-26 11:55:32 +0200

    test: upload-artifact

--- tests/test_sample.py
+++ tests/test_sample.py
@@ -4,5 +4,5 @@ from src.sample import add
 def test_sample_success():
     assert add(5, 5) == 10
 
-def test_sample_failure():
-    assert add(5, 5) == 55
+# def test_sample_failure():
+#     assert add(5, 5) == 55
