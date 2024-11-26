11f1e8353d2863aed0bb29404eb1d2d7785930f1
saisaravanagn <saisaravanagn@gmail.com>
2024-11-26 16:18:24 +0100

    Convert sample.py to Python2
    Converted sample.py to Python2

--- src/sample.py
+++ src/sample.py
@@ -1,5 +1,5 @@
-def add(a: int, b: int) -> int:
+def add(a, b): 
     return a + b
 
-if __name__ == "__main__":
-    print("Exploring GitHub Actions...")
+if __name__ == "__main__": 
+    print("Executing Jenkins Pipeline...")
